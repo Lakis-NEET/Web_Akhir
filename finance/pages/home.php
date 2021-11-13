12eae05a91717d428b538000d06a4609ce9019ad
agistarakha <agistarakhasnc@gmail.com>
2021-11-13 18:50:04 +0700

    responsive test

--- finance/pages/home.php
+++ finance/pages/home.php
@@ -25,26 +25,63 @@ if (defined('GELANG') === false) {
         </div>
     <?php } ?>
 </div>
-<div class="row pt-3 pb-2 mb-3 border-bottom">
-    <h2 class="">Latest Manga</h2>
-</div>
-<div class="row justify-content-center">
-    <!-- row-cols-xl-3 row-cols-md-2 row-cols-sm-1 -->
-    <?php
-    for ($i = 0; $i < 9; $i++) {
-    ?>
-        <div class="col-auto mb-2">
-            <div class="card p-2 card-latest">
-                <img src="https://via.placeholder.com/100x150" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h6 class="fs-6">Title</h6>
-                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-                    <!-- <a href="#" class="btn btn-dark">Read</a> -->
-                </div>
-            </div>
+<div class="row">
+    <div class="col-8">
+        <div class="row pt-3 pb-2 mb-3 border-bottom">
+            <h2 class="">Latest Manga</h2>
         </div>
-    <?php } ?>
+        <div class="row justify-content-center">
+            <!-- row-cols-xl-3 row-cols-md-2 row-cols-sm-1 -->
+            <?php
+            for ($i = 0; $i < 9; $i++) {
+            ?>
+                <div class="col-auto mb-2">
+                    <div class="card p-2 card-latest">
+                        <img src="https://via.placeholder.com/100x150" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h6 class="fs-6">Title</h6>
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                            <!-- <a href="#" class="btn btn-dark">Read</a> -->
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="row pt-3 pb-2 mb-3 border-bottom">
+            <h2 class="">Top Manga</h2>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                for ($i = 1; $i < 5; $i++) {
+                ?>
+                    <tr class="border-1">
+                        <th scope="row"><?= $i ?></th>
+                        <td> <img src="https://via.placeholder.com/50x75" class="card-img-top" alt="...">
+                        </td>
+                        <td class="d-flex flex-column border-0">
+                            <p>Title</p>
+                            <p>Genre</p>
+                            <p>Score</p>
+                        </td>
+                    </tr>
+                <?php } ?>
+
+            </tbody>
+        </table>
+
+    </div>
 </div>
+
 <div class="">
     <nav aria-label="Page navigation example" class="">
         <ul class="pagination">
