eb6bdf149bfba24bdbb2f10af4f150b131b76097
agistarakha <agistarakhasnc@gmail.com>
2021-12-01 12:24:37 +0700

    Random fix

--- finance/pages/filter_genre.php
+++ finance/pages/filter_genre.php
@@ -1,25 +1,24 @@
 <?php
-if(defined('GELANG')===false){
+if (defined('GELANG') === false) {
     //tidak memiliki gelang
     die("Anda tidak berhak membuka file ini secara langsung");
 }
 
-$comic_array=[];
+$comic_array = [];
 if (isset($_GET['genre_id'])) {
     //page tidak ditemukan
     include 'genre.php';
-    $id=$_GET['genre_id'];
-    $gen=mysqli_query($connection, "SELECT * FROM comic_genre WHERE genre_id = '$id'");   
-    while($filter=mysqli_fetch_assoc($gen)){
-        $comic_id=$filter["comic_id"];
-        $comic_sql=mysqli_query($connection, "SELECT * FROM comic_list WHERE comic_id = '$comic_id'");
+    $id = $_GET['genre_id'];
+    $gen = mysqli_query($connection, "SELECT * FROM comic_genre WHERE genre_id = '$id'");
+    while ($filter = mysqli_fetch_assoc($gen)) {
+        $comic_id = $filter["comic_id"];
+        $comic_sql = mysqli_query($connection, "SELECT * FROM comic_list WHERE comic_id = '$comic_id'");
         array_push($comic_array, mysqli_fetch_assoc($comic_sql));
     }
-}
-else if (isset($_GET['keyword'])) {
-    $name=$_GET['keyword'];
-    $comic_sql=mysqli_query($connection,"SELECT * FROM comic_list WHERE comic_title LIKE '%$name%'");
-    while ($gan=mysqli_fetch_assoc($comic_sql)) {
+} else if (isset($_GET['keyword'])) {
+    $name = $_GET['keyword'];
+    $comic_sql = mysqli_query($connection, "SELECT * FROM comic_list WHERE comic_title LIKE '%$name%'");
+    while ($gan = mysqli_fetch_assoc($comic_sql)) {
         array_push($comic_array, $gan);
     }
 }
@@ -33,15 +32,12 @@ else if (isset($_GET['keyword'])) {
     <?php
     foreach ($comic_array as $comic_list) {
     ?>
-    <div class="">
         <div class="card p-2 card-popular card-home">
             <img src="<?php echo $comic_list["comic_cover"] ?>" class="card-img-top" alt="...">
             <div class="card-body">
-                <a class="" style="font-size: 0.8rem;"
-                    href="?page=details&id=<?php echo $comic_list['comic_id']?>"><?php echo $comic_list["comic_title"] ?></a>
+                <a class="" style="font-size: 0.8rem;" href="?page=details&id=<?php echo $comic_list['comic_id'] ?>"><?php echo $comic_list["comic_title"] ?></a>
                 <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
             </div>
         </div>
-    </div>
     <?php } ?>
 </div>
